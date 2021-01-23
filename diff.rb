57b75a312fe2a32310fc4cd9583ad26ce4a95a10
Colin Key <colin.m.key@gmail.com>
2021-01-23 16:35:35 -0700

    Adds diff script

---
+++ diff.rb
@@ -0,0 +1,4 @@
+require './scripts/copyroo.rb'
+
+differ = Copyroo.new
+differ.diff_versions
--- scripts/copyroo.rb
+++ scripts/copyroo.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 
 class Copyroo
   def initialize
-    @excluded_files = ['.git', 'scripts', 'install.rb', 'update.rb', 'README.md']
+    @excluded_files = ['.git', 'scripts', 'diff.rb', 'install.rb', 'update.rb', 'README.md']
     @files = Dir.children(Dir.pwd).select { |f| !@excluded_files.include?(f) }
   end
 
@@ -19,5 +19,24 @@ class Copyroo
       FileUtils.copy File.join(Dir.home, f), f
     end
     puts 'Copying complete. Please `git push`'
+  end
+
+  def diff_versions
+    @files.each do |f|
+      installed = File.open(file_location(f), 'r')
+      local = File.open(f, 'r')
+      identical = installed.read == local.read
+      if identical
+        puts "No changes found in #{f}"
+      else
+        puts "Changes found in #{f}"
+      end
+    end
+  end
+
+  private
+
+  def file_location(file)
+    File.join(Dir.home, file)
   end
 end
