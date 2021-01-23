4c07a944bf1a7b90bdd9811be328c37e17fc7f41
Colin Key <colin.m.key@gmail.com>
2021-01-23 16:43:59 -0700

    Some cleanup

--- scripts/copyroo.rb
+++ scripts/copyroo.rb
@@ -2,41 +2,41 @@ require 'fileutils'
 
 class Copyroo
   def initialize
-    @excluded_files = ['.git', 'scripts', 'diff.rb', 'install.rb', 'update.rb', 'README.md']
-    @files = Dir.children(Dir.pwd).select { |f| !@excluded_files.include?(f) }
+    excluded_files = ['.git', 'scripts', 'diff.rb', 'install.rb', 'update.rb', 'README.md']
+    @files = Dir.children(Dir.pwd).select { |f| !excluded_files.include?(f) }
   end
 
   def get_updates
     @files.each do |f|
-      puts "Copying #{f} from git repo..."
-      FileUtils.copy f, File.join(Dir.home, f)
+      puts "Copying #{f} to home..."
+      FileUtils.copy f, home_location(f)
     end
     puts 'Copying complete. Enjoy!'
   end
 
   def push_updates
     @files.each do |f|
-      FileUtils.copy File.join(Dir.home, f), f
+      puts "Copying #{f} from home..."
+      FileUtils.copy home_location(f), f
     end
     puts 'Copying complete. Please `git push`'
   end
 
   def diff_versions
     @files.each do |f|
-      installed = File.open(file_location(f), 'r')
-      local = File.open(f, 'r')
-      identical = installed.read == local.read
+      identical = File.open(home_location(f), 'r').read == File.open(f, 'r')
       if identical
         puts "No changes found in #{f}"
       else
         puts "Changes found in #{f}"
       end
     end
+    puts 'Diffing complete.'
   end
 
   private
 
-  def file_location(file)
+  def home_location(file)
     File.join(Dir.home, file)
   end
 end
