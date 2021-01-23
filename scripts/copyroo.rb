344cab451e5af4bc09212624fb02fed6f2dd374f
Colin Key <colin.m.key@gmail.com>
2021-01-23 11:14:26 -0700

    Update copy scripts

--- scripts/copyroo.rb
+++ scripts/copyroo.rb
@@ -9,15 +9,14 @@ class Copyroo
   def get_updates
     @files.each do |f|
       puts "Copying #{f} from git repo..."
-      FileUtils.copy f, "../#{f}"
+      FileUtils.copy f, "~/#{f}"
     end
     puts 'Copying complete. Enjoy!'
   end
 
   def push_updates
     @files.each do |f|
-      puts "Pushing #{f} to git repo..."
-      FileUtils.copy "../#{f}", f
+      FileUtils.copy "~/#{f}", f
     end
     puts 'Copying complete. Please `git push`'
   end
