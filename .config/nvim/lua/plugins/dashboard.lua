99b9a02acddf0bf1c6d468b05a49178909635e7c
Colin Key <colin.m.key@gmail.com>
2025-02-07 21:43:41 -0700

    feat: revamp dashboard

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -7,7 +7,19 @@ return {
 			config = {
 				week_header = {
 					enable = true
-				}
+				},
+				shortcut = {
+					{ desc = '󰊳 Update packages', group = '@property', action = 'Lazy update', key = 'u' },
+					{ desc = ' File picker', group = 'Number', action = 'Telescope find_files', key = 'f' },
+					{
+						desc = ' dotfiles',
+						group = '@property',
+						action = 'Telescope find_files cwd=~/Dev/dotfiles',
+						key = 'd',
+					},
+				},
+				packages = { enable = false },
+				footer = {}
 			}
 		}
 	end,
